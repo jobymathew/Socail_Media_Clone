0dd1525442b2d1146e2c99045f99219dcc397f7c
Joby Mathew <jobymathew.mec@gmail.com>
2020-04-15 22:01:18 +0530

    base template and login url setup

--- simple_social_clone/urls.py
+++ simple_social_clone/urls.py
@@ -19,5 +19,7 @@ from accounts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/',include('accounts.urls',namespace='accounts')),
+    path('accounts/',include('django.contrib.auth.urls')),
     path('',views.HomePage.as_view(),name='home'),
 ]
