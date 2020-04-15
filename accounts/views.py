b1ef7ad52bc0b18fa2d5038087a91b6ee6ee6557
Joby Mathew <jobymathew.mec@gmail.com>
2020-04-15 21:25:00 +0530

    added sign up form and models

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView, CreateView
+from django.urls import reverse_lazy
+from . import forms
 # Create your views here.
+
+class HomePage(TemplateView):
+    template_name = 'index.html'
+
+class SignUp(CreateView):
+    form_class = forms.UserCreateFrom
+    success_url = reverse_lazy('login')
+    template_name = 'accounts/signup.html'
+    
+    
